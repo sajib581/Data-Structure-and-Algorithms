ce23353dc5013ece3ec832d8791cfc27ac3cef27
sajib581 <sajibdas581@gmail.com>
2020-06-24 15:41:34 +0600

    neighber

--- GRAPH/First/1.cpp
+++ GRAPH/First/1.cpp
@@ -7,12 +7,14 @@ int adj[100][100] ;
 
 int main()
 {
+    freopen("input.txt","r",stdin) ;
+
     int node,edge ;
-    cout<<"Enter number of nodes : " ;
+
     cin>>node ;
-    cout<<"Enter number of edges : " ;
+
     cin>>edge ;
-    cout<<"Enter all values"<<endl ;
+
     int n1,n2 ;
 
     for(int i = 0 ;i<edge;i++){
@@ -20,12 +22,20 @@ int main()
             adj[n1][n2]=1 ;
             adj[n2][n1]=1 ;
     }
-    for(int row=0;row<node;row++){
+
+    /*for(int row=0;row<node;row++){
         for(int column=0;column<node;column++){
             cout<<adj[row][column]<<"  " ;
         }
         cout<<endl;
+    }*/
+
+    for(int i=0;i<node;i++){
+        if(adj[0][i]==1){
+            cout<<i<<" " ;
+        }
     }
+
     return 0 ;
 
 }
